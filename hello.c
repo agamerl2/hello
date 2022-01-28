11c330650dc7f0ff04b28e53f6c90aa2d76f46a0
agamerl2 <agamerl2@huskers.unl.edulslsls ~lslsgit config --global user.email agamerl2@huskers.unl.edugit config --global user. name Adam>
2022-01-28 08:22:59 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Adam Gamerl
+ *         agamerl@unomaha.edu
+ * Date: 2020/03/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Adam Gamerl\n");
+
+  return 0;
+}
